f2547f3efcebfe54eecc20189d6bcbd60cff6534
Umair Abdullah <umaharabdullah@gmail.com>
2024-09-13 21:28:04 +0500

    first commit

---
+++ kindergarten_garden.py
@@ -0,0 +1,31 @@
+DEFAULT_STUDENTS = (
+    "Alice",
+    "Bob",
+    "Charlie",
+    "David",
+    "Eve",
+    "Fred",
+    "Ginny",
+    "Harriet",
+    "Ileana",
+    "Joseph",
+    "Kincaid",
+    "Larry",
+)
+
+PLANTS = {
+    "C": "Clover",
+    "G": "Grass",
+    "R": "Radishes",
+    "V": "Violets",
+}
+
+
+class Garden(object):
+    def __init__(self, diagram, students=DEFAULT_STUDENTS):
+        self.lines = diagram.splitlines()
+        self.students = sorted(students)
+
+    def plants(self, student):
+        index = self.students.index(student) * 2
+        return [PLANTS[p[i]] for p in self.lines for i in (index, index + 1)]
